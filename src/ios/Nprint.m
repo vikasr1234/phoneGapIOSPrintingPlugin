e75319a9a7ef4ab4bddd758d9f999501827a83ba
Vikas Rawat <vikas.rawat@irdsoft.com>
2017-10-04 18:16:49 +0530

    Migration from ePOS-Print SDK with Epson ePOS SDK

--- src/ios/Nprint.m
+++ src/ios/Nprint.m
@@ -1,10 +1,10 @@
 
 #import <Cordova/CDVAvailability.h>
-#import "ePOS-Print.h"
-#import "ePOSBluetoothConnection.h"
+#import "ePOS2.h"
 #import "NPrint.h"
 
-@interface NPrinter ()
+@interface NPrinter () <Epos2DiscoveryDelegate, Epos2PtrReceiveDelegate>
+    Epos2Printer *printer_;
     - (void) sendError:(int)errorStatus textForShow:(NSString*)text command:(CDVInvokedUrlCommand*)command;
     - (void) sendError:(NSString*)text command:(CDVInvokedUrlCommand*)command;
 @end
@@ -19,32 +19,26 @@
 
 - (void) getAvailablePrinters:(CDVInvokedUrlCommand*)command
 {
-    int find_printers_status = EPSONIO_OC_SUCCESS;
-    find_printers_status = [EpsonIoFinder start:EPSONIO_OC_DEVTYPE_BLUETOOTH FindOption:nil];
-    
-    [self sendError:find_printers_status textForShow:@"error during printer search" command:command];
-    
-    int get_printers_list_status = EPSONIO_OC_SUCCESS;
-    
-    NSArray *printerList_ = [[NSArray alloc]initWithArray:
-                             [EpsonIoFinder getDeviceInfoList:&get_printers_list_status
-                                            FilterOption:EPSONIO_OC_PARAM_DEFAULT]];
-    
-    [EpsonIoFinder stop];
-    
-    if ( [printerList_ count] < 1 ) {
+    Epos2FilterOption *option = [Epos2FilterOption alloc] init];
+    [Epos2Discovery start:option delegate:self];
+
+    // searching
+    [Epos2Discovery stop];
+
+    Epos2DeviceInfo* targetPrinter = [NSObject alloc];
+
+    if (targetPrinter == nil) {
         [self sendError:@"printers not found" command:command];
     } else {
-        
         NSMutableArray *listForSend = [[NSMutableArray alloc] init];
-        
+
         for (int i = 0; i < [printerList_ count]; i++) {
-            [listForSend addObject:[[printerList_ objectAtIndex:i] printerName]];
+            [listForSend addObject:[targetPrinter printerName]];
         }
-        
+
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
                                                          messageAsArray:listForSend];
-        
+
         [self.commandDelegate sendPluginResult:pluginResult
                               callbackId:command.callbackId];
     }
@@ -53,101 +47,82 @@
 
 - (void) print:(CDVInvokedUrlCommand*)command
 {
-    
+    [printer_ setReceiveEventDelegate:self];
+    [targetPrinter_ setDiscoveryEventDelegate:self];
+
     NSArray * arguments = [command arguments];
     NSString *contentForPrint = [arguments objectAtIndex:0];
     NSString *chosenPrinterName = [arguments objectAtIndex:1];
-    
-    int find_printers_status = EPSONIO_OC_SUCCESS;
-    find_printers_status = [EpsonIoFinder start:EPSONIO_OC_DEVTYPE_BLUETOOTH FindOption:nil];
-    
-    [self sendError:find_printers_status textForShow:@"error during printer search" command:command];
-    
-    int get_printers_list_status = EPSONIO_OC_SUCCESS;
-    
-    NSArray *printerList_ = [[NSArray alloc]initWithArray:
-                            [EpsonIoFinder getDeviceInfoList:&get_printers_list_status
-                                           FilterOption:EPSONIO_OC_PARAM_DEFAULT]];
-    
-    [EpsonIoFinder stop];
-    
+
     NSString* deviceName = nil;
     NSString* printerName = nil;
-    
-    for (int i = 0; i < [printerList_ count]; i++) {
-        
-        if ([chosenPrinterName isEqualToString:[[printerList_ objectAtIndex:i] printerName]]) {
-            deviceName = [[printerList_ objectAtIndex:i] deviceName];
-            printerName = [[printerList_ objectAtIndex:i] printerName];
-        }
+    Epos2DeviceInfo* targetPrinter = [NSObject alloc];
+
+    if (targetPrinter != nil) {
+        deviceName = [targetPrinter deviceName];
+        printerName = [targetPrinter printerName];
     }
-    
+
     if (deviceName == nil || printerName == nil) {
         [self sendError:@"printer is unavalibale" command:command];
     } else {
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
                                                          messageAsString:@"success"];
-        
+
         [self.commandDelegate sendPluginResult:pluginResult
                               callbackId:command.callbackId];
     }
-    
-    //Initialize an EposBuilder class instance
-    id builder = [[EposBuilder alloc] initWithPrinterModel:printerName Lang:EPOS_OC_MODEL_ANK];
-    if ( builder == nil ) {
-        [self sendError:@"builder initialize error" command:command];
-    }
-    
+
     //Create a print document
-    int errorStatus = EPOS_OC_SUCCESS;
-    errorStatus = [builder addTextLang: EPOS_OC_LANG_EN];
-    errorStatus = [builder addTextSmooth: EPOS_OC_TRUE];
-    errorStatus = [builder addTextFont: EPOS_OC_FONT_A];
-    errorStatus = [builder addTextSize: 3 Height: 3];
-    errorStatus = [builder addText: contentForPrint];
-    errorStatus = [builder addCut: EPOS_OC_CUT_FEED];
-    
-    //Initialize an EposPrint class instance
-    id printer = [[EposPrint alloc] init];
+    int errorStatus = EPOS2_SUCCESS;
+    errorStatus = [printer_ addTextLang: EPOS2_LANG_EN];
+    errorStatus = [printer_ addTextSmooth: EPOS2_TRUE];
+    errorStatus = [printer_ addTextFont: EPOS2_FONT_A];
+    errorStatus = [printer_ addTextSize: 3 Height: 3];
+    errorStatus = [printer_ addText: contentForPrint];
+    errorStatus = [printer_ addCut: EPOS2_CUT_FEED];
+
+    //Initialize an Epos2Printer class instance
+    Epos2Printer printer = [[Epos2Printer alloc] initWithPrinterSeries:EPOS2_TM_T88 lang:EPOS2_MODEL_ANK];
     unsigned long status;
-    
+
     //Send a print document
     if (printer == nil) {
         [self sendError:@"printer initialize error" command:command];
     }
-    
+
     //<Start communication with the printer>
-    errorStatus = [printer openPrinter:EPSONIO_OC_DEVTYPE_BLUETOOTH
-                           DeviceName:deviceName Enabled:EPOS_OC_TRUE
-                           Interval:EPOS_OC_PARAM_DEFAULT Timeout:EPOS_OC_PARAM_DEFAULT];
-    
+
+    errorStatus = [printer connect:deviceName
+                           timeout:EPOS2_PARAM_DEFAULT];
+
     //<Send data>
-    errorStatus = [printer sendData:builder Timeout:10000 Status:&status];
-    
+    errorStatus = [printer_ sendData:builder Timeout:10000 Status:&status];
+
     [self sendError:errorStatus textForShow:@"Failure to send data to printer" command:command];
-    
+
     //<Delete the command buffers>
-    if ((status & EPOS_OC_ST_PRINT_SUCCESS) == EPOS_OC_ST_PRINT_SUCCESS) {
+    if ((status & EPOS2_ST_PRINT_SUCCESS) == EPOS2_ST_PRINT_SUCCESS) {
         errorStatus = [builder clearCommandBuffer];
     }
-    
+
     //<End communication with the printer>
-    errorStatus = [printer closePrinter];
-    
+    errorStatus = [printer disconnect];
+
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
                                                      messageAsString:@"success"];
-    
+
     [self.commandDelegate sendPluginResult:pluginResult
                           callbackId:command.callbackId];
-    
+
 }
 
 - (void) sendError:(int)errorStatus textForShow:(NSString*)text command:(CDVInvokedUrlCommand*)command
 {
-    if (errorStatus != EPOS_OC_SUCCESS) {
+    if (errorStatus != EPOS2_SUCCESS) {
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
                                                          messageAsString:[NSString stringWithFormat:@"%@%i", text, errorStatus]];
-        
+
         [self.commandDelegate sendPluginResult:pluginResult
                               callbackId:command.callbackId];
     }
@@ -157,9 +132,13 @@
 {
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
                                                      messageAsString:text];
-    
+
     [self.commandDelegate sendPluginResult:pluginResult
                           callbackId:command.callbackId];
+}
+
+- (void) onDiscovery:(Epos2DeviceInfo *)deviceInfo {
+    NSString* target = [deviceInfo getTarget];
 }
 
 @end
